3d8e6aad836d0b0b042ee98845637a0d91d9d34f
BJ Rutledge <86386474+bj-rutledge@users.noreply.github.com>
2022-09-10 18:25:35 -0700

    Configured for azure

--- azure-functions/api/form-fill/index.js
+++ azure-functions/api/form-fill/index.js
@@ -1,15 +1,67 @@
-
+/**
+ * Created by BJ Rutledge
+ * Date: 2022-09-09
+ * Living with Conviction.org
+ * Azure function. Get Filled form.
+ */
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+  if (!req.body) {
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+      status: 500,
+      body: {
+        error: new Error('No body was received.'),
+      },
     };
-}
+  }
+
+  const data = req.body;
+  switch (body.formType) {
+    case 'gr39':
+      //do gr39
+      return;
+    
+    //do other forms when needed! 
+
+    default:
+      context.res = {
+        status: 500,
+        body: {
+          error: new Error('No body was received.'),
+        },
+      };
+      return; 
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// context.log('JavaScript HTTP trigger function processed a request.');
+
+// const name = (req.query.name || (req.body && req.body.name));
+// const responseMessage = name
+//     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+//     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+// context.res = {
+//     // status: 200, /* Defaults to 200 */
+//     body: responseMessage
+// };
